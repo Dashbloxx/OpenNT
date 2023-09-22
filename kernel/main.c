0cb1a103924bf778b43327e0ffc22ced91c7cb15
Dashbloxx <dashbloxx@gmail.com>
2023-09-21 20:41:23 -0400

    Add an example of using the serial driver...

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,10 @@
+#include "serial.h"
+
 int main()
 {
+    /* Initialize COM1 serial port, and then print the character 'a' to COM1... */
+    serial_initialize(COM1);
+    serial_write(COM1, 'a');
+
     while(1) { asm volatile("hlt"); }
 }

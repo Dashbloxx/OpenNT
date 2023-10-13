5e17e755f060ec04e3c96d89f5788d64eccc6e52
Dashbloxx <dashbloxx@gmail.com>
2023-10-12 20:56:13 -0400

    Paging works successfully!

--- kernel/memory/virtmem.c
+++ kernel/memory/virtmem.c
@@ -1,46 +1,32 @@
-#include <stdbool.h>
+#include <stdint.h>
 
 #include "virtmem.h"
-#include "physmem.h"
-#include "../utility/string.h"
+#include "paging.h"
 
-bool is_paging_enabled = false;
-
-page_directory_t page_directory[1024] __attribute__((aligned(4096)));
-page_table_t page_tables[1024] __attribute__((aligned(4096)));
+uint32_t page_directory[1024] __attribute__((aligned(4096)));
+uint32_t first_page_table[1024] __attribute__((aligned(4096)));
 
 void virtmem_initialize()
 {
-    int i;
-    uint32_t cr0;
-
-    memset((uint8_t *)page_directory, 0, sizeof(page_directory_t) * 1024);
-    memset((uint8_t *)page_tables, 0, sizeof(page_table_t) * 1024);
-
-    for(i = 0; i < 1024; i++)
+    for(int i = 0; i < 1024; i++)
     {
-        page_directory[i].read_write = 1;
-        page_directory[i].user = 1;
+        // This sets the following flags to the pages:
+        //   Supervisor: Only kernel-mode can access them
+        //   Write Enabled: It can be both read from and written to
+        //   Not Present: The page table is not present
+        page_directory[i] = 0x00000002;
+    }
+ 
+    //we will fill all 1024 entries in the table, mapping 4 megabytes
+    for(unsigned int i = 0; i < 1024; i++)
+    {
+        // As the address is page aligned, it will always leave 12 bits zeroed.
+        // Those bits are used by the attributes ;)
+        first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
     }
 
-    for(i = 0; i < 1024; i++)
-    {
-        page_tables[i].present = 1;
-        page_tables[i].read_write = 1;
-        page_tables[i].user = 1;
-        page_tables[i].frame = (i * PAGE_SIZE) >> 12;
-    }
+    page_directory[0] = ((unsigned int)first_page_table) | 3;
 
-    page_directory[0].present = 1;
-    page_directory[0].accessed = 0;
-    page_directory[0].user = 1;
-    page_directory[0].frame = 0x11a;
-
-    asm volatile("mov %0, %%cr3" ::"r"(page_directory));
-
-    asm volatile("mov %%cr0, %0": "=r"(cr0));
-    cr0 = cr0 | 0x80000000;
-    asm volatile("mov %0, %%cr0" ::"r"(cr0));
-
-    is_paging_enabled = true;
+    load_page_directory((uint32_t *)&page_directory);
+    enable_paging();
 }
